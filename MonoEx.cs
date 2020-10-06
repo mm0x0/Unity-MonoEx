afb0f9ee3cb5b726670792b07524fdd32ce08934
Unknown <mm0x0b@gmail.com>
2020-10-07 05:20:00 +0900

    GetOnlyOneComponent <T> を追加

--- MonoEx.cs
+++ MonoEx.cs
@@ -17,6 +17,20 @@ public class MonoEx
 		return (T) component;
 	}
 
+	// シーン上に１つだけあるコンポーネントを取得
+	public static T GetOnlyOneComponent <T> () where T : Component
+	{
+		T [] components = GameObject.FindObjectsOfType <T> ();
+
+		if (components.Length == 0)
+		   Debug.LogError (typeof (T) + " がシーン上にないよ");
+
+		if (components.Length >= 2)
+		   Debug.LogError (typeof (T) + " を2つ以上置いちゃだめだよ");
+
+		return components [0];
+	}
+
 	// シーン上に2つ以上同じコンポーネントがあるか判別
 	public static bool DoubleComponentsInScene<T> ()
 		where T : Component
